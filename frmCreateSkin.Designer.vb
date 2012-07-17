292b8b281a7406d8bf7dcaef866b9f880b6812f9
tiben20b <tiben20b@gmail.com>
2012-07-17 23:28:56 +0000

    Added image previewer, added creation of window when creating a skin and fixed loading of texture

--- frmCreateSkin.Designer.vb
+++ frmCreateSkin.Designer.vb
@@ -25,26 +25,26 @@ Partial Class frmCreateSkin
         Me.cmdCancel = New System.Windows.Forms.Button()
         Me.cmdCreate = New System.Windows.Forms.Button()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.lblExtension = New System.Windows.Forms.Label()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.cmdAddLanguage = New System.Windows.Forms.Button()
-        Me.Doubletextbox1 = New RichTextBoxEx.doubletextbox()
-        Me.txtPlatform = New RichTextBoxEx.TextButton()
         Me.txtDebugging = New RichTextBoxEx.TextButton()
         Me.txtEffectSlowDown = New RichTextBoxEx.TextButton()
         Me.txtDefaultTheme = New RichTextBoxEx.TextButton()
         Me.txtDefaultResWide = New RichTextBoxEx.TextButton()
         Me.txtDefaultRes = New RichTextBoxEx.TextButton()
         Me.txtPoint = New RichTextBoxEx.TextButton()
+        Me.lblExtension = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Doubletextbox2 = New RichTextBoxEx.doubletextbox()
+        Me.Doubletextbox1 = New RichTextBoxEx.doubletextbox()
+        Me.cmdAddLanguage = New System.Windows.Forms.Button()
+        Me.txtPlatform = New RichTextBoxEx.TextButton()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.txtGuiVersion = New RichTextBoxEx.TextButton()
         Me.txtProvider = New RichTextBoxEx.TextButton()
         Me.txtName = New RichTextBoxEx.TextButton()
         Me.txtVersion = New RichTextBoxEx.TextButton()
         Me.txtId = New RichTextBoxEx.TextButton()
-        Me.Doubletextbox2 = New RichTextBoxEx.doubletextbox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
@@ -81,66 +81,6 @@ Partial Class frmCreateSkin
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(332, 221)
         Me.Panel1.TabIndex = 11
-        '
-        'lblExtension
-        '
-        Me.lblExtension.AutoSize = True
-        Me.lblExtension.Location = New System.Drawing.Point(8, 5)
-        Me.lblExtension.Name = "lblExtension"
-        Me.lblExtension.Size = New System.Drawing.Size(56, 13)
-        Me.lblExtension.TabIndex = 0
-        Me.lblExtension.Text = "Extension:"
-        '
-        'Panel2
-        '
-        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Panel2.Controls.Add(Me.Label3)
-        Me.Panel2.Controls.Add(Me.Label2)
-        Me.Panel2.Controls.Add(Me.Doubletextbox2)
-        Me.Panel2.Controls.Add(Me.Doubletextbox1)
-        Me.Panel2.Controls.Add(Me.cmdAddLanguage)
-        Me.Panel2.Controls.Add(Me.txtPlatform)
-        Me.Panel2.Controls.Add(Me.Label1)
-        Me.Panel2.Location = New System.Drawing.Point(344, 13)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(378, 467)
-        Me.Panel2.TabIndex = 12
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(3, 12)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(55, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Language"
-        '
-        'cmdAddLanguage
-        '
-        Me.cmdAddLanguage.Location = New System.Drawing.Point(65, 2)
-        Me.cmdAddLanguage.Name = "cmdAddLanguage"
-        Me.cmdAddLanguage.Size = New System.Drawing.Size(75, 23)
-        Me.cmdAddLanguage.TabIndex = 22
-        Me.cmdAddLanguage.Text = "Add"
-        Me.cmdAddLanguage.UseVisualStyleBackColor = True
-        '
-        'Doubletextbox1
-        '
-        Me.Doubletextbox1.Location = New System.Drawing.Point(6, 43)
-        Me.Doubletextbox1.Name = "Doubletextbox1"
-        Me.Doubletextbox1.Size = New System.Drawing.Size(320, 28)
-        Me.Doubletextbox1.TabIndex = 23
-        '
-        'txtPlatform
-        '
-        Me.txtPlatform.Align = System.Windows.Forms.HorizontalAlignment.Right
-        Me.txtPlatform.ButtonBorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.txtPlatform.LabelText = "platform"
-        Me.txtPlatform.Location = New System.Drawing.Point(-2, 417)
-        Me.txtPlatform.Name = "txtPlatform"
-        Me.txtPlatform.Size = New System.Drawing.Size(378, 28)
-        Me.txtPlatform.TabIndex = 21
-        Me.txtPlatform.TextboxText = "all"
         '
         'txtDebugging
         '
@@ -208,6 +148,91 @@ Partial Class frmCreateSkin
         Me.txtPoint.TabIndex = 13
         Me.txtPoint.TextboxText = "xbmc.gui.skin"
         '
+        'lblExtension
+        '
+        Me.lblExtension.AutoSize = True
+        Me.lblExtension.Location = New System.Drawing.Point(8, 5)
+        Me.lblExtension.Name = "lblExtension"
+        Me.lblExtension.Size = New System.Drawing.Size(56, 13)
+        Me.lblExtension.TabIndex = 0
+        Me.lblExtension.Text = "Extension:"
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.Doubletextbox2)
+        Me.Panel2.Controls.Add(Me.Doubletextbox1)
+        Me.Panel2.Controls.Add(Me.cmdAddLanguage)
+        Me.Panel2.Controls.Add(Me.txtPlatform)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Location = New System.Drawing.Point(344, 13)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(378, 467)
+        Me.Panel2.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 224)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 26
+        Me.Label3.Text = "Description"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Summary"
+        '
+        'Doubletextbox2
+        '
+        Me.Doubletextbox2.Location = New System.Drawing.Point(6, 236)
+        Me.Doubletextbox2.Name = "Doubletextbox2"
+        Me.Doubletextbox2.Size = New System.Drawing.Size(320, 28)
+        Me.Doubletextbox2.TabIndex = 24
+        '
+        'Doubletextbox1
+        '
+        Me.Doubletextbox1.Location = New System.Drawing.Point(6, 43)
+        Me.Doubletextbox1.Name = "Doubletextbox1"
+        Me.Doubletextbox1.Size = New System.Drawing.Size(320, 28)
+        Me.Doubletextbox1.TabIndex = 23
+        '
+        'cmdAddLanguage
+        '
+        Me.cmdAddLanguage.Location = New System.Drawing.Point(65, 2)
+        Me.cmdAddLanguage.Name = "cmdAddLanguage"
+        Me.cmdAddLanguage.Size = New System.Drawing.Size(75, 23)
+        Me.cmdAddLanguage.TabIndex = 22
+        Me.cmdAddLanguage.Text = "Add"
+        Me.cmdAddLanguage.UseVisualStyleBackColor = True
+        '
+        'txtPlatform
+        '
+        Me.txtPlatform.Align = System.Windows.Forms.HorizontalAlignment.Right
+        Me.txtPlatform.ButtonBorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.txtPlatform.LabelText = "platform"
+        Me.txtPlatform.Location = New System.Drawing.Point(-2, 417)
+        Me.txtPlatform.Name = "txtPlatform"
+        Me.txtPlatform.Size = New System.Drawing.Size(378, 28)
+        Me.txtPlatform.TabIndex = 21
+        Me.txtPlatform.TextboxText = "all"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Language"
+        '
         'txtGuiVersion
         '
         Me.txtGuiVersion.Align = System.Windows.Forms.HorizontalAlignment.Right
@@ -262,31 +287,6 @@ Partial Class frmCreateSkin
         Me.txtId.Size = New System.Drawing.Size(322, 28)
         Me.txtId.TabIndex = 5
         Me.txtId.TextboxText = ""
-        '
-        'Doubletextbox2
-        '
-        Me.Doubletextbox2.Location = New System.Drawing.Point(6, 236)
-        Me.Doubletextbox2.Name = "Doubletextbox2"
-        Me.Doubletextbox2.Size = New System.Drawing.Size(320, 28)
-        Me.Doubletextbox2.TabIndex = 24
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(3, 31)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(50, 13)
-        Me.Label2.TabIndex = 25
-        Me.Label2.Text = "Summary"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(3, 224)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(60, 13)
-        Me.Label3.TabIndex = 26
-        Me.Label3.Text = "Description"
         '
         'frmCreateSkin
         '
