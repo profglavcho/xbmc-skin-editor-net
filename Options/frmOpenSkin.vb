691ffccceab52e558bb72b5c93b142e6940353a5
tiben20b <tiben20b@gmail.com>
2012-07-18 22:21:30 +0000

    fixed open skin

--- Options/frmOpenSkin.vb
+++ Options/frmOpenSkin.vb
@@ -1,18 +1,26 @@
 ﻿Public Class frmOpenSkin
-    Dim skinfolder As New List(Of String)
+
+    Dim skinfolder As New List(Of IO.DirectoryInfo)
 
     Private Sub frmOpenSkin_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim addonfolder As String = "C:\Users\ben\AppData\Roaming\XBMC\addons"
-        For Each Dir As String In IO.Directory.GetDirectories(addonfolder)
+        Dim strfolder As String = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)
+        If IO.Directory.Exists(strfolder + "\xbmc\addons") = False Then
+            Exit Sub
+        End If
+        Dim addonFolder As New IO.DirectoryInfo(strfolder + "\xbmc\addons")
 
-            If Dir.Contains("skin.") Then
+        For Each Dir As IO.DirectoryInfo In addonFolder.GetDirectories()
+
+            If Dir.Name.Contains("skin.") Then
                 'its a skin so add it
+                'TODO
+                'don't get the name of the folder but load it from the addon.xml
                 skinfolder.Add(Dir)
             End If
         Next
-        For Each skin As String In skinfolder
+        For Each skin As IO.DirectoryInfo In skinfolder
             Try
-                lstSkinList.Items.Add(skin)
+                lstSkinList.Items.Add(skin.Name)
             Catch ex As Exception
                 Debug.Print(ex.Message)
             End Try
@@ -30,7 +38,9 @@
     End Sub
 
     Private Sub cmdOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdOpen.Click
-        My.Forms.frmMain.LoadSkin(lstSkinList.SelectedItem().ToString())
+        If lstSkinList.SelectedIndex() > -1 = False Then Exit Sub
+
+        frmMain.LoadSkin(skinfolder(lstSkinList.SelectedIndex).FullName)
         Me.Close()
     End Sub
 End Class
